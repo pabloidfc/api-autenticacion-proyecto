056765dfc5479c1067225549c647465011a29c12
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-09-11 15:36:27 -0300

    configurando laravel passport
    	- creando funciones para generar tokens

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,30 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function Register(Request $req){
+
+        $validacion = Validator::make($req -> all(),[
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        if($validacion -> fails())
+            return $validacion -> errors();
+
+        return $this -> createUser($req);
+        
+    }
+
+    private function createUser($req){
+        $user = new User();
+        $user -> name     = $req -> post("name");
+        $user -> email    = $req -> post("email");
+        $user -> password = Hash::make($req -> post("password"));   
+        $user -> save();
+        return $user;
+    }
+
     public function ValidarToken(Request $req){
         return auth('api') -> user();
     }
@@ -19,6 +43,4 @@ class AuthController extends Controller
         $req -> user() -> token() -> revoke();
         return ['msg' => 'Token Revoked'];
     }
-
-
 }

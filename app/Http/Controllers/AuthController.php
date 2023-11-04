440f6d428ff23f0ff4d111b9327776858aff5214
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-11-04 13:40:14 -0300

    configurando cors
    - eliminando funcionalidades innecesarias
    - actualizando libreria fruitcake

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,41 +11,6 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function works() {
-        return response("FUNCIONA!!!", 200);
-    }
-
-    public function Register(Request $req){
-
-        $validacion = Validator::make($req -> all(),[
-            "ci"        => "required|string|max:8",
-            "nombre"    => "required|alpha|max:40",
-            "nombre2"   => "nullable|alpha|max:40",
-            "apellido"  => "required|alpha|max:40",
-            "apellido2" => "required|alpha|max:40",
-            "email"     => "required|email|unique:users",
-            "password"  => "required|confirmed"
-        ]);
-
-        if($validacion -> fails())
-            return response($validacion -> errors(), 400);
-
-        return $this -> createUser($req);
-    }
-
-    private function createUser($req){
-        $user = new User();
-        $user -> ci        = $req -> input("ci");
-        $user -> nombre    = $req -> input("nombre");
-        $user -> nombre2   = $req -> input("nombre2");
-        $user -> apellido  = $req -> input("apellido");
-        $user -> apellido2 = $req -> input("apellido2");
-        $user -> email     = $req -> input("email");
-        $user -> password = Hash::make($req -> input("password"));   
-        $user -> save();
-        return $user;
-    }
-
     public function ValidarToken(Request $req){
         return auth("api") -> user();
     }
